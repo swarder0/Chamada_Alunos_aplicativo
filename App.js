6a3d5cf5ca97908fd6626897b28259c25414dace
ecampos14 <132613659+swarder0@users.noreply.github.com>
2023-09-16 17:38:40 -0300

    Atualizações design do aplicativo

--- App.js
+++ App.js
@@ -6,29 +6,36 @@ import { MaterialCommunityIcons } from '@expo/vector-icons';
 
 const styles = StyleSheet.create({
   safeArea: {
-    flex: 1,
-    paddingTop: 20,
-    margin: 16,
+    paddingTop: 40,
+    margin: 20,
   },
   listItemHeader:{
-    marginBottom: 10,
+    marginBottom: 20,
+    fontSize: 16,
+    fontWeight: '600',
+    alignSelf: 'center',
+    backgroundColor: '#191970',
+    color: '#F0E68C',
+    borderRadius: 20,
+    padding: 10,    
   },
-  listItemPlaceholder:{    
-    marginBottom: 10, 
+  listItemPlaceholder:{  
+    marginBottom:10,
     borderWidth: 1,
-    borderColor: '#4F4F4F', 
+    borderColor: '#4F4F4F',
     borderRadius: 10,
     padding: 6,
+    backgroundColor: '#FFFAF0',
   },
   listItem: {
     marginBottom: 16,
     padding: 8,
-    backgroundColor: '#fff',
-    borderWidth: 1,
+    borderWidth: 1.5,
     borderColor: '#4F4F4F',
+    backgroundColor: '#FFFAF0',
     borderRadius: 20,
-    flexDirection: 'row',  
-    alignItems: 'center',  
+    flexDirection: 'row',
+    alignItems: 'center',
     justifyContent: 'space-between',
     height: 80,
   },
@@ -44,13 +51,34 @@ const styles = StyleSheet.create({
   },
   serieHeader: {
     fontWeight: 'bold',
-  },
-  ButtonAddAluno:{
-    borderRadius: 40,
+    marginBottom: 15,
   },
   CheckList:{
     flexDirection: 'row',
     gap: 10,
+    color: '#DCDCDC',
+  },
+  BackColor:{
+    backgroundColor: '#D3D3D3',
+    flex: 1,
+  },
+  btnAdicionarAluno:{
+    backgroundColor: '#191970',
+    borderColor: 'black',
+    borderWidth: 1,
+    borderRadius: 20,
+    padding: 10,
+    alignSelf: 'center',
+    margin: 30,
+  },
+  btnTexto: {
+    color: '#F0E68C',
+    fontSize: 16,
+    fontWeight: '600',
+  },
+  TextHeader: {
+    fontSize: 16,
+    fontWeight: '600',
   },
 });
 
@@ -99,62 +127,72 @@ const excluirAluno = (serie, index) => {
 };
 
 return (
-  <SafeAreaView style={styles.safeArea}>
-    <View >
-      <Text style={styles.listItemHeader}>Presença de Alunos: </Text>
-      <Text>Nome do Aluno: </Text> 
-      <TextInput style={styles.listItemPlaceholder}
-        placeholder=""
-        value={nomeAluno}
-        onChangeText={(text) => setNomeAluno(text)}
-      />
-      <Text>Serie do Aluno: </Text>
-      <TextInput style={styles.listItemPlaceholder}
-        placeholder=""
-        value={serieAluno}
-        onChangeText={(text) => setSerieAluno(text)}
-      />
-      <Button title="Adicionar Aluno" onPress={adicionarAluno} />
-      <FlatList
-        data={Object.entries(alunos)}
-        keyExtractor={(item) => item[0]}
-        renderItem={({ item }) => (
-          <>
-            <Text style={styles.serieHeader}>Série: {item[0]}</Text>
-            {item[1].map((aluno, index) => (
-              <View key={index} style={styles.listItem}>
-                 <View style={{ flex: 1, gap: 10 }}>
-                  <Text>Nome: {aluno.nome}</Text>
-                  <View style={styles.CheckList}>
-                    <Text>Ida: </Text>
-                    <TouchableOpacity onPress={() => setbtnitemState(!btnIda)}>
-                      {!btnIda && <MaterialCommunityIcons name="checkbox-blank-circle-outline" size={24} color="black" />}
-                      {btnIda && <MaterialCommunityIcons name="checkbox-marked-circle" size={24} color="black" />}
-                    </TouchableOpacity>
-                    <Text>Volta: </Text>
-                    <TouchableOpacity onPress={() => setbtnitemState1(!btnVolta)}>
-                      {!btnVolta && <MaterialCommunityIcons name="checkbox-blank-circle-outline" size={24} color="black" />}
-                      {btnVolta && <MaterialCommunityIcons name="checkbox-marked-circle" size={24} color="black" />}
-                    </TouchableOpacity>
-                    <Text>Faltou: </Text>
-                    <TouchableOpacity onPress={() => setbtnitemState2(!btnFalta)}>
-                      {btnFalta && <MaterialCommunityIcons name="checkbox-marked" size={24} color="black" />}
-                      {!btnFalta && <MaterialCommunityIcons name="checkbox-blank-outline" size={24} color="black" />}
+  <View style={styles.BackColor}> 
+    <View style={{backgroundColor: '#DAA520', height: 380, borderBottomWidth: 2, borderColor: '#1C1C1C',}}>
+      <SafeAreaView style={styles.safeArea}>
+        <View >
+          <Text style={styles.listItemHeader}>Presença de Alunos: </Text>
+          <Text style={styles.TextHeader}>Nome do Aluno: </Text> 
+          <TextInput style={styles.listItemPlaceholder}
+            placeholder=""
+            value={nomeAluno}
+            onChangeText={(text) => setNomeAluno(text)}
+          />
+          <Text style={styles.TextHeader}>Serie do Aluno: </Text>
+          <TextInput style={styles.listItemPlaceholder}
+            placeholder=""
+            value={serieAluno}
+            onChangeText={(text) => setSerieAluno(text)}
+          />
+          <TouchableOpacity style={styles.btnAdicionarAluno} onPress={adicionarAluno}>
+            <Text style = {styles.btnTexto}> Adicionar Aluno</Text>
+          </TouchableOpacity>
+          <FlatList
+            data={Object.entries(alunos)}
+            keyExtractor={(item) => item[0]}
+            renderItem={({ item }) => (
+              <>
+                <Text style={styles.serieHeader}>Série: {item[0]}</Text>
+                {item[1].map((aluno, index) => (
+                  <View key={index} style={styles.listItem}>
+                    <View style={{ flex: 1, gap: 10,}}>
+                      <View style={{flexDirection: 'row',}}>
+                        <Text style={{fontWeight: 'bold',}}>Nome: </Text>
+                        <Text>{aluno.nome}</Text>
+                      </View>                    
+                      <View style={styles.CheckList}>
+                        <Text style={styles.TextHeader}>Ida: </Text>
+                        <TouchableOpacity onPress={() => setbtnitemState(!btnIda)}>
+                          {!btnIda && <MaterialCommunityIcons name="checkbox-blank-circle-outline" size={24} color="black" />}
+                          {btnIda && <MaterialCommunityIcons name="checkbox-marked-circle" size={24} color="black" />}
+                        </TouchableOpacity>
+                        <Text style={styles.TextHeader}>Volta: </Text>
+                        <TouchableOpacity onPress={() => setbtnitemState1(!btnVolta)}>
+                          {!btnVolta && <MaterialCommunityIcons name="checkbox-blank-circle-outline" size={24} color="black" />}
+                          {btnVolta && <MaterialCommunityIcons name="checkbox-marked-circle" size={24} color="black" />}
+                        </TouchableOpacity>
+                        <Text style={styles.TextHeader}>Faltou: </Text>
+                        <TouchableOpacity onPress={() => setbtnitemState2(!btnFalta)}>
+                          {btnFalta && <MaterialCommunityIcons name="checkbox-marked" size={24} color="black" />}
+                          {!btnFalta && <MaterialCommunityIcons name="checkbox-blank-outline" size={24} color="black" />}
+                        </TouchableOpacity>
+                      </View>
+                    </View>
+                    <TouchableOpacity
+                      style={styles.deleteButton}
+                      onPress={() => excluirAluno(item[0], index)}>
+                      <Ionicons name="ios-close" size={24} color="red" />
                     </TouchableOpacity>
                   </View>
-                </View>
-                <TouchableOpacity
-                  style={styles.deleteButton}
-                  onPress={() => excluirAluno(item[0], index)}>
-                  <Ionicons name="ios-close" size={24} color="red" />
-                </TouchableOpacity>
-              </View>
-            ))}
-          </>
-        )}
-      />
-    </View>
-  </SafeAreaView>
+                ))}
+              </>
+            )}
+          />
+        </View>
+      </SafeAreaView>
+    </View> 
+  </View>
+  
 );
 };
 
--- README.md
+++ README.md
@@ -4,3 +4,11 @@ aplicativo para realizar uma chamada de alunos para transporte escolar, feito co
 
 foi feito uma aplicação utilizando react native e suas bibliotecas para a ornanização do aplicativo.
 uma aplicação simples para o usuario, basta ele inserir o nome e serie do aluno e marcar se os alunos estão realizando o transporte.
+
+**Atualizações 1.0**
+
+Foi inserido uma melhoria para o botão de presença/ausência, modificado para "Ida: , Volta: e Faltou: , para ser feito a verificação se o aluno que utiliza do transporte escolar está indo, voltando ou se ele faltou no dia em questão, foi feito essa mudança pensando em melhorias para o app, condiderando que os alunos que utilizando o transporte escolar, podem apenas ir ou voltar com a van, ou se tenham faltado, para ser mais facil ter um controle dos alunos que foram em um dia específico"
+
+**Atualizações 1.1**
+
+Realizado melhorias visuais no front do aplicativo, para auxiliar o usuario a identificar melhor onde deve ser inseridos os dados e como realizar a chamada de ida e volta, também fiz melhorias no botão de ação "Adicionar Alunos", antes utilizava um "buttom" mas sem poder ser customizado, agora foi feito com um "TouchableOpacity", funcionando da mesma forma mas, que facilita a modificações na aparencia dentro do app.
